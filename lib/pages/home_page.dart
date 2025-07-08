34597c74550ae4eb3bf774f80a785a45c221c44d
D14LB3L0 <diegobdev2024@gmail.com>
2025-07-08 16:18:06 +0100

    added crud methods for Scans table

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:qr_reader/models/scan_model.dart';
 import 'package:qr_reader/pages/pages.dart';
 import 'package:qr_reader/providers/providers.dart';
 
